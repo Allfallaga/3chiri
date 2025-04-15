5475230257857ca2961f0e19816644ec89ef1000
Allfallaga <62994489+Allfallaga@users.noreply.github.com>
2025-04-15 21:31:05 +0200

    Update test_routes.py

--- test_routes.py
+++ test_routes.py
@@ -1,1 +1,23 @@
+import pytest
 
+def test_inscription_get(client):
+    """Test GET request to inscription page"""
+    response = client.get('/inscription')
+    assert response.status_code == 200
+
+def test_connexion_get(client):
+    """Test GET request to login page"""
+    response = client.get('/connexion')  
+    assert response.status_code == 200
+
+def test_inscription_post(client):
+    """Test user registration"""
+    data = {
+        'username': 'testuser',
+        'password': 'testpass',
+        'confirm': 'testpass',
+        'social_link': 'http://test.com',
+        'followers': '100'
+    }
+    response = client.post('/inscription', data=data)
+    assert response.status_code == 302  # Redirect after successful registration
